57f36364c1ce16df74ced7b6a80ad630a3c0952e
Liam Hanrahan <liamthanrahan@gmail.com>
2021-01-29 15:36:59 +1100

    Converted all JS files to TS (#2)

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <p>
+        This page has a static route that is loaded manually. All other pages
+        are loaded dynamically on build.
+      </p>
+    </Layout>
+  );
+}
